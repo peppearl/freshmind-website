6dce4be80f01abb1ce39a230b4d9297ebd216d3a
Gabrielle Geroudet <gabrielle.geroudet@gmail.com>
2022-06-18 21:03:30 +0200

    feat: adding functionnalities ✨

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <Header/>
+  <Functionnalities/>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import Functionnalities from "./components/Functionnalities";
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Functionnalities
   }
 }
 </script>
