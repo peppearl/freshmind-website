ddefb1c3ad70da71dc37cd1444e2f0dd02d39460
Gabrielle Geroudet <gabrielle.geroudet@gmail.com>
2022-06-18 21:29:31 +0200

    feat: adding footer ✨

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <Header/>
   <Functionnalities/>
+  <Footer/>
 </template>
 
 <script>
 import Header from './components/Header.vue'
 import Functionnalities from "./components/Functionnalities";
+import Footer from "@/components/Footer";
 
 export default {
   name: 'App',
   components: {
     Header,
-    Functionnalities
+    Functionnalities,
+    Footer
   }
 }
 </script>
