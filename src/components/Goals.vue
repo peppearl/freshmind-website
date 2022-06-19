c1ab5b7257ce9aef73f22411f34a7cda3acbb395
Gabrielle Geroudet <gabrielle.geroudet@gmail.com>
2022-06-19 19:12:19 +0200

    fix: header + style ☄️

--- src/components/Goals.vue
+++ src/components/Goals.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="text-black">
-    <div class="bg-red h-36 text-center shadow-l">
-      <h2 class="text-white uppercase pt-12 text-5xl font-extrabold">Nos objectifs</h2>
+    <div class="bg-red h-32 text-center shadow-l">
+      <h2 class="text-white uppercase pt-10 text-5xl font-extrabold">Nos objectifs</h2>
     </div>
     <div class="flex mx-40 my-14 font-semibold justify-around">
       <div class="flex flex-col items-center">
@@ -17,15 +17,15 @@
           <img src="../assets/img/icone-organisation.png" alt="Charge mentale icône" class="w-12 relative top-22 left-23">
         </span>
         <div class="mt-8">
-          <p class="text-center">Vous apportez une<br>meilleure organisation</p>
+          <p class="text-center">Vous apporter une<br>meilleure organisation</p>
         </div>
       </div>
       <div class="flex flex-col items-center">
         <span class="bg-blue w-24 h-24 rounded-full">
-          <img src="../assets/img/icone-app.png" alt="Charge mentale icône" class="w-8 relative top-22 left-32">
+          <img src="../assets/img/icone-app.png" alt="Charge mentale icône" class="w-8 relative top-22 left-32px">
         </span>
         <div class="mt-8">
-          <p class="text-center">Gérez votre quotidien<br>sur une seule application</p>
+          <p class="text-center">Gérer votre quotidien<br>sur une seule application</p>
         </div>
       </div>
     </div>
