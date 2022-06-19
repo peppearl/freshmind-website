ffef2b980e3cd4ba453983a8f09c5382fc153493
Gabrielle Geroudet <gabrielle.geroudet@gmail.com>
2022-06-19 18:41:05 +0200

    feat: Adding our goals ✨️

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <Header/>
   <Functionnalities/>
+  <Goals/>
   <Footer/>
 </template>
 
@@ -8,13 +9,15 @@
 import Header from './components/Header.vue'
 import Functionnalities from "./components/Functionnalities";
 import Footer from "@/components/Footer";
+import Goals from "@/components/Goals";
 
 export default {
   name: 'App',
   components: {
     Header,
     Functionnalities,
-    Footer
+    Footer,
+    Goals
   }
 }
 </script>
